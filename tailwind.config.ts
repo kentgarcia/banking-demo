d2c38280645a0b34a4a680466d688f113d9037c1
Kent Garcia <kentgarcia.dgl@gmail.com>
2025-06-25 09:43:36 +0000

    reduce the speed of the spinning.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -112,7 +112,7 @@ export default {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         blob: 'blob 7s infinite',
-        'border-spin': 'border-spin 4s linear infinite',
+        'border-spin': 'border-spin 8s linear infinite',
       },
     },
   },
