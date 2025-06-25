9bc137432d65d320ff462a4b6c3f6ce26ae58f34
Kent Garcia <kentgarcia.dgl@gmail.com>
2025-06-25 09:49:36 +0000

    still super fast. i think the spinning is not changing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -112,7 +112,7 @@ export default {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         blob: 'blob 7s infinite',
-        'border-spin': 'border-spin 20s linear infinite',
+        'border-spin': 'border-spin 60s linear infinite',
       },
     },
   },
